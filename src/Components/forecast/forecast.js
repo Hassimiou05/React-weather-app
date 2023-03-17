7dc31fb1b02530c49d0c26c9f539ae2012740794
Hassimiou05 <djpabloescobar0@gmail.com>
2023-03-17 13:53:54 +0100

    Test validé

---
+++ src/Components/forecast/forecast.js
@@ -0,0 +1,66 @@
+import { Accordion, AccordionItem, AccordionItemPanel, AccordionItemHeading, AccordionItemButton } from "react-accessible-accordion";
+import './forecast.css';
+const WEEK_DAYS = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+
+const Forecast = ({ data }) => {
+
+    const dayInAWeek = new Date().getDay();
+    const forecastDays = WEEK_DAYS.slice(dayInAWeek, WEEK_DAYS.length).concat(WEEK_DAYS.slice(0, dayInAWeek));
+
+
+
+    return (
+        <>
+            <label className="title">Daily</label>
+            <Accordion allowZeroExpanded>
+                {data.list.splice(0, 7).map((item, idx) => (
+                    <AccordionItem key={idx}>
+                        <AccordionItemHeading>
+                            <AccordionItemButton>
+                                <div className="daily-item">
+                                    <img alt="weather" className="icon-small" src="" />
+                                    <label className="day">{forecastDays[idx]}</label>
+                                    <label className="description">{item.weather[0].description}</label>
+                                    <label className="min-max">
+                                        {Math.round(item.main.temp_min)}°C /{" "}
+                                        {Math.round(item.main.temp_max)}°C
+                                    </label>
+                                </div>
+                            </AccordionItemButton>
+                        </AccordionItemHeading>
+                        <AccordionItemPanel>
+                            <div className="daily-details-grid">
+                                <div className="daily-details-grid-item">
+                                    <label>Pression</label>
+                                    <label>{item.main.pressure}hPa</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Humidité</label>
+                                    <label>{item.main.humidity}%</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Nuage</label>
+                                    <label>{item.clouds.all}%</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Vitesse du vent:</label>
+                                    <label>{item.wind.speed} m/s</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>sea level</label>
+                                    <label>{item.main.sea_level}m</label>
+                                </div>
+                                <div className="daily-details-grid-item">
+                                    <label>Ressentis</label>
+                                    <label>{Math.round(item.main.feels_like)}°C</label>
+                                </div>
+                            </div>
+                        </AccordionItemPanel>
+                    </AccordionItem>
+                ))}
+
+            </Accordion>
+        </>
+    )
+}
+export default Forecast;
---
+++ src/api.js
@@ -0,0 +1,14 @@
+
+export const geoApiOptions = {
+    method: 'GET',
+    url: 'https://wft-geo-db.p.rapidapi.com/v1/geo/cities',
+    headers: {
+        'X-RapidAPI-Key': '2f3d6f5adbmshd0f81793ca070e2p16287fjsn125b1c72f277',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/";
+export const WEATHER_API_KEY = "b79a5f2ec17e4ea0e01efde846a29944";
